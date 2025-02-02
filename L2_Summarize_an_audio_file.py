37fb956d1a0651eedda99c5cddf66324980144d0
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-02-01 21:19:24 -0300

    Elimino las celdas del notebook de Jupyter

--- L2_Summarize_an_audio_file.py
+++ L2_Summarize_an_audio_file.py
@@ -4,10 +4,6 @@
 # # Lesson 2: Summarize an audio file
 
 # ### Import all needed packages
-
-# In[ ]:
-
-
 import os
 from IPython.display import Audio
 import boto3
@@ -18,58 +14,16 @@ from jinja2 import Template
 
 
 # ### Let's start with transcribing an audio file
-
-# In[ ]:
-
-
 audio = Audio(filename="dialog.mp3")
 display(audio)
-
-
-# In[ ]:
-
-
 s3_client = boto3.client('s3', region_name='us-west-2')
-
-
-# In[ ]:
-
-
 bucket_name = os.environ['BucketName']
 
-
-# In[ ]:
-
-
 file_name = 'dialog.mp3'
-
-
-# In[ ]:
-
-
 s3_client.upload_file(file_name, bucket_name, file_name)
-
-
-# In[ ]:
-
-
 transcribe_client = boto3.client('transcribe', region_name='us-west-2')
-
-
-# In[ ]:
-
-
 job_name = 'transcription-job-' + str(uuid.uuid4())
-
-
-# In[ ]:
-
-
 job_name
-
-
-# In[ ]:
-
 
 response = transcribe_client.start_transcription_job(
     TranscriptionJobName=job_name,
@@ -83,20 +37,13 @@ response = transcribe_client.start_transcription_job(
     }
 )
 
-
-# In[ ]:
-
-
 while True:
     status = transcribe_client.get_transcription_job(TranscriptionJobName=job_name)
     if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
         break
     time.sleep(2)
+
 print(status['TranscriptionJob']['TranscriptionJobStatus'])
-
-
-# In[ ]:
-
 
 if status['TranscriptionJob']['TranscriptionJobStatus'] == 'COMPLETED':
     
@@ -134,60 +81,23 @@ if status['TranscriptionJob']['TranscriptionJobStatus'] == 'COMPLETED':
 
 # ### Now, let's use an LLM
 
-# In[ ]:
-
-
 bedrock_runtime = boto3.client('bedrock-runtime', region_name='us-west-2')
-
-
-# In[ ]:
-
 
 with open(f'{job_name}.txt', "r") as file:
     transcript = file.read()
 
-
-# In[ ]:
-
-
 get_ipython().run_cell_magic('writefile', 'prompt_template.txt', 'I need to summarize a conversation. The transcript of the \nconversation is between the <data> XML like tags.\n\n<data>\n{{transcript}}\n</data>\n\nThe summary must contain a one word sentiment analysis, and \na list of issues, problems or causes of friction\nduring the conversation. The output must be provided in \nJSON format shown in the following example. \n\nExample output:\n{\n    "sentiment": <sentiment>,\n    "issues": [\n        {\n            "topic": <topic>,\n            "summary": <issue_summary>,\n        }\n    ]\n}\n\nWrite the JSON output and nothing more.\n\nHere is the JSON output:\n')
-
-
-# In[ ]:
-
 
 with open('prompt_template.txt', "r") as file:
     template_string = file.read()
-
-
-# In[ ]:
-
 
 data = {
     'transcript' : transcript
 }
 
-
-# In[ ]:
-
-
 template = Template(template_string)
-
-
-# In[ ]:
-
-
 prompt = template.render(data)
-
-
-# In[ ]:
-
-
 print(prompt)
-
-
-# In[ ]:
-
 
 kwargs = {
     "modelId": "amazon.titan-text-express-v1",
@@ -205,22 +115,7 @@ kwargs = {
     )
 }
 
-
-# In[ ]:
-
-
 response = bedrock_runtime.invoke_model(**kwargs)
-
-
-# In[ ]:
-
-
 response_body = json.loads(response.get('body').read())
 generation = response_body['results'][0]['outputText']
-
-
-# In[ ]:
-
-
 print(generation)
-
